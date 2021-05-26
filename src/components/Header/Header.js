ea18fbdaa7bf1a04422d08234367c9e839bd482b
Konstantin Khoroshilov <manly41142009@yandex.ru>
2021-05-26 19:49:45 +0300

    feat: добавить компоненты Promo, Main, Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from 'react-router-dom';  
+import "./Header.css";
+import logo from "../../images/logo.svg";
+
+function Header({ place }) {
+  if (place === "Promo") {
+    return (
+      <header className="header">
+        <img className="header__logo" alt="Логотип" src={logo} />
+        <nav className="header__link-container">
+          <Link className="header__register-link" to="/signup">Регистрация</Link>
+          <Link className="header__login-link" to="/signin">Войти</Link>
+        </nav>
+      </header>
+    );
+  }
+  
+}
+export default Header;
